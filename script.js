214c8a6562af469888e02d99af49029000594484
Siddique-Ahmed <itssiddique786@gmail.com>
2024-03-09 04:13:52 -0800

    chapter 2

---
+++ script.js
@@ -0,0 +1,35 @@
+var firstName = "Siddique ";
+var lastName = "Ahmed";
+
+alert("Name : " + firstName + lastName);
+
+var yourAge = "Age :" + 23;
+alert(yourAge)
+
+var courseName = "Web And Mobile App Development"
+
+alert("Course Name : " + courseName)
+
+var pizza = "pizza "
+var pizz = "pizz "
+var piz = "piz "
+var pi = "pi "
+var p = "p"
+
+alert(pizza + "\n" + pizz + "\n"+ piz + "\n"+ pi + "\n" + p)
+
+var eMail = "itssiddique786@gmail.com"
+
+alert( "My Email : " + eMail)
+
+var book = "A smarter way to learn JavaScript"
+
+alert("I am trying to learn JavaScript with the help of this Book : " + book);
+
+var htmlContent = "Yah! I can write HTML content through JavaScript";
+
+document.write(htmlContent)
+
+var Variable = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+
+alert(Variable)
